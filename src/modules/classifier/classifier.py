8d254b5ad145709dd922b4e8063c24ccdf7dcd6e
jaivalis <johnaivalis@gmail.com>
2014-06-20 18:01:42 +0200

    added weights to weakClassifier train()

--- src/modules/classifier/classifier.py
+++ src/modules/classifier/classifier.py
@@ -9,92 +9,125 @@ class StrongClassifier(object):
         self.beta = beta
         self.gamma = gamma
         self.layers = layers
-        self.classifiers = []  # len(classifiers) = T
+        self.all_classifiers = []  # len(classifiers) = T
+
+        self.classifiers = []
+        self.theta_a = None
+        self.theta_b = None
 
         #########    Parzen window technique    #########
         # Phase1:  Create all possible weak classifiers #
         for feature in feature_holder.get_features():
             wc = WeakClassifier(feature)
-            self.classifiers.append(wc)
-        self.classifiers = self.classifiers[-10:len(self.classifiers)]  #TODO remove this, testing
-        print "Initialized %d weak classifiers." % (len(self.classifiers))
+            self.all_classifiers.append(wc)
+        self.all_classifiers = self.all_classifiers[-10:len(self.all_classifiers)]  #TODO remove this, testing
+        print "Initialized %d weak classifiers." % (len(self.all_classifiers))
 
         # # Phase2: Compute the classifier response for all the training patches
         # samples = 0
         # cll = 0
-        # for classifier in self.classifiers:
+        # for classifier in self.all_classifiers:
         #     cll += 1
         #     while True:
         #         for patch in training_stream.get_random_patches():
         #             samples += 1
-        #             classifier.train(patch)
+        #             classifier.store_response(patch)
         #
         #         if samples > sample_count + 1:
         #             break
         #     print "Done extracting feature #%d: " % cll + str(classifier)
         #     classifier.plot_gaussian()
 
-    # Phase3: Algorithm2: Learning with bootstrapping
+        # Phase3: Algorithm2: Learning with bootstrapping
         self.learn_with_bootstrapping(sample_count)
 
     def learn_with_bootstrapping(self, sample_count):
-        """
-        Algorithm2 Sochman
-        ==================
+        """ Algorithm2 Sochman
         Outputs the strong classifier with the theta_a, theta_b of the weak classifiers updated
         """
-        tr = self.training_stream.extract_training_patches(sample_count)
+        s = self.training_stream.extract_training_patches(sample_count)
 
         # initialize weights
         weighted_patches = {}
-        for patch in tr:
-            weighted_patches[patch] = 1. / len(tr)
+        for patch in s:
+            weighted_patches[patch] = 1. / len(s)
 
         A = (1 - self.beta) / self.alpha
         B = self.beta / (1 - self.alpha)
 
-        for t in range(self.layers):
+        for t in range(self.layers+1):
             # choose the weak classifier with the minimum error
             h_t = self._find_optimal_weak_classifier(weighted_patches)
+            # add it to the strong classifier
+            self.classifiers.append(h_t)
 
-            # estimate the likelihood ratio R_t eq. 19
-
-            # find thresholds for chosen classifier
+            # find decision thresholds for the strong classifier
+            self._tune_thresholds(t)
 
             # throw away training samples for which
 
             # sample new training data
         return
 
+    def _tune_thresholds(self, layer):
+        """
+        :return:
+        """
+
+        pass
+
     def _find_optimal_weak_classifier(self, weighted_patches):
-        ret = None
-        for wc in self.classifiers:
-            patches = weighted_patches.keys()
+        """ Returns the weak classifier with the least error
+        :param weighted_patches:
+        :return:
+        """
+        # patches = weighted_patches.keys()
+        for wc in self.all_classifiers:
+            wc.train(weighted_patches)
+        self.all_classifiers.sort()
+        # self.all_classifiers[0].plot_gaussian()
 
-            wc.train(patches)
+        return self.all_classifiers[0]
 
-        self.classifiers.sort()
-        assert ret is not None
-        return self.classifiers[0]
+    def h_t(self, t, x):
+        """ H_t(x) returns the summation of the responses of the first t weak classifiers.
+        :param t: Layer count
+        :param x: Patch to be classified
+        :return: H_t(x)
+        """
+        ret = 0
+        for wc in self.classifiers[0:t]:
+            ret += wc.classify(x)
+        return ret
 
     def classify(self, patch):
-        """
-        Implementation of Algorithm 1 Sochman
+        """ Implementation of Algorithm 1 Sochman.
         :param patch: Patch to be classified
         :return: +1, -1
         """
-        for cl in self.classifiers:
-            label = cl.classify(patch)
-            if label != 0:
-                return label
-        if self.H(patch) > self.gamma:
+        for t in range(0, self.layers):
+            h_t = h_t(t, patch)
+            wc = self.classifiers[t]
+            if h_t >= wc.theta_b:
+                return +1
+            if h_t <= wc.theta_a:
+                return -1
+        if self.h_t(self.layers, patch) > self.gamma:
             return +1
-        return -1
+        else:
+            return -1
 
-    def H(self, patch):
-        ret = 0
-        for wc in self.classifiers:
-            ret += wc.h(patch)
+    def get_r(self, patches, t):
+        """
+
+        :param patches:
+        :param t:
+        :return:
+        """
+        for t in range(0, self.layers):
+            pass
+
+        return self.error
 
     def train(self, frame_count):
         frames_processed = 0
--- src/modules/classifier/weakClassifier.py
+++ src/modules/classifier/weakClassifier.py
@@ -5,28 +5,35 @@ from modules.util.math_f import pdf_gaussian, plot_gaussians
 class WeakClassifier(object):
     def __init__(self, feature):
         self.feature = feature
-        self.best_thresh_sm = -1
-        self.best_thresh_gr = -1
-        self.w = -1
-        self.h = None
-        self.error = None
+        self.error = np.float64(1.)
         # used to calculate the standard deviation
-        self.annotated_responses = None  # TODO get rid of this ?
-        self.pos_gmm = None  # Gaussian Mixture model for the +1 class
-        self.neg_gmm = None  # Gaussian Mixture model for the -1 class
+        self.annotated_responses = None
+        self.threshold = None
+        # The sign of the dominant class left of the threshold
+        self.dominant_left = None
 
     def classify(self, patch):
-        """
-        :return: values: -1, 0, +1
+        """ Returns [the probability of the patch being of the +1 class]
+        :return:
         """
         response = self.feature.apply(patch.crop)
-        if response < self.theta_a:
-            return 1
-        elif response > self.theta_b:
-            return -1
-        return 0
+        if response < self.threshold:
+            epsilon = 1. / 2.*self.annotated_responses[self.annotated_responses[:, 1] < self.threshold]
+            pos = self.annotated_responses[self.annotated_responses[:, 1] == self.dominant_left]
+            cor = pos[pos[:, 0] < self.threshold]
+            inc = pos[pos[:, 0] > self.threshold]
+            return .5 * np.log(cor + epsilon/(inc + epsilon))
+        else:
+            epsilon = 1. / 2.*self.annotated_responses[self.annotated_responses[:, 1] < self.threshold]
+            pos = self.annotated_responses[self.annotated_responses[:, 1] == -self.dominant_left]
+            cor = pos[pos[:, 0] > self.threshold]
+            inc = pos[pos[:, 0] < self.threshold]
+            return .5 * np.log(cor + epsilon/(inc + epsilon))
 
-    def train(self, patch):
+    def classify_weighted_patches(self, weighted_patches):
+        pass
+
+    def store_response(self, patch):
         """ For a given patch saves the response in self.responses
         """
         response = self.feature.apply(patch.crop)
@@ -37,61 +44,67 @@ class WeakClassifier(object):
             self.annotated_responses = np.array([response, label])
         else:
             self.annotated_responses = np.vstack((self.annotated_responses, [response, label]))
-        sigma = np.std(self.annotated_responses)
 
-        n = len(self.annotated_responses)
-        h = 1.144 * sigma * n ** (-1/5)
-        xs = np.linspace(0, 255)
-
-        # gaussian = pdf_gaussian(xs, mu=response, sigma=sigma)
-        # if patch.label == 1:
-        #     self.pos_gmm = append_gaussian(self.pos_gmm, gaussian, np.linspace(-20, 300))
-        # elif patch.label == -1:
-        #     self.neg_gmm = append_gaussian(self.pos_gmm, gaussian, np.linspace(-20, 300))
-
-    def train(self, patches):
+    def train(self, weighted_patches):
         """ Find the threshold that produces the lowest error
         """
-        for p in patches:
+        for p, w in weighted_patches.iteritems():
             response = self.feature.apply(p.crop)
             true_label = p.label
             # append to the responses
             if self.annotated_responses is None:
-                self.annotated_responses = np.array([response, true_label])
+                self.annotated_responses = np.array([response, true_label, w])
             else:
-                self.annotated_responses = np.vstack((self.annotated_responses, [response, true_label]))
+                self.annotated_responses = np.vstack((self.annotated_responses, [response, true_label, w]))
 
-        smallest = 0
-        greatest = 0
-        best_thresh_sm = best_thresh_gr = -1
         response_values = self.annotated_responses[:, 0]
-        for t in range(min(response_values)-1, max(response_values)+1):
-            thresh_pos_candidate = t
-            responses_below_threshold = sum(response_values < thresh_pos_candidate) / len(response_values)
-            responses_above_threshold = sum(response_values > thresh_pos_candidate) / len(response_values)
-            if responses_below_threshold > smallest:
-                smallest = responses_below_threshold
-                best_thresh_sm = thresh_pos_candidate
-                self.error = 1 - smallest
-            if responses_above_threshold > greatest:
-                greatest = responses_above_threshold
-                best_thresh_gr = thresh_pos_candidate
-                self.error = 1 - greatest
+        pos_response_values = self.annotated_responses[self.annotated_responses[:, 1] == 1]
+        neg_response_values = self.annotated_responses[self.annotated_responses[:, 1] == -1]
 
-            if 1-smallest != 1-greatest:
-                print 'not the same'
+        # find which class occupies the left and which the right portion of the responses
+        response_values.sort()
+        median = response_values[len(self.annotated_responses) / 2]
 
-        assert best_thresh_sm != -1 and best_thresh_gr != -1
-        self.best_thresh_sm = best_thresh_sm
-        self.best_thresh_gr = best_thresh_gr
+        weight_pos_left = 0
+        weight_pos_right = 0
+        for [response, true_label, w] in self.annotated_responses:
+            if true_label != +1:
+                continue
+            if response < median:
+                weight_pos_left += w
+            else:
+                weight_pos_right += w
+
+        if weight_pos_left < weight_pos_right:
+            self.dominant_left = -1
+            left = neg_response_values
+            right = pos_response_values
+        else:
+            self.dominant_left = +1
+            left = pos_response_values
+            right = neg_response_values
+
+        for t in response_values:
+            thr = t + .5
+            if thr > max(response_values):
+                continue
+
+            misclassified_left = left[left[:, 0] > thr]
+            misclassified_right = right[right[:, 0] < thr]
+
+            misclassified = sum(misclassified_left[:, 2]) + sum(misclassified_right[:, 2])
+
+            err = misclassified / sum(self.annotated_responses[:, 2])
+
+            if err < self.error:
+                self.error = err
+                self.threshold = thr
 
     def get_error(self):
         return self.error
 
     def plot_gaussian(self):
-        """
-        Plots the mixture of Gaussians split into two classes (+, -)
-        """
+        """ Plots the mixture of Gaussians split into two classes (+, -) """
         sigma = np.std(self.annotated_responses)
 
         n = len(self.annotated_responses)
@@ -100,13 +113,12 @@ class WeakClassifier(object):
         plot_gaussians(self.annotated_responses, sigma, h)
 
     def __gt__(self, other):
-        return self.error < other.error
+        return self.error > other.error
 
     def __eq__(self, other):
         return self.w == other.w and self.error == other.error
 
     def __str__(self):
         ret = "Feature: {" + str(self.feature) + "}"
-        ret += " theta_A:" + str(self.theta_a) + " theta_B:"
-        ret += str(self.theta_b) + " w:" + str(self.w)
+        # ret += " error:" + str(self.error) + " theta_B:"
         return ret
