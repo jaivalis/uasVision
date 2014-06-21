94a550771b19f81c8124cb59c3c4eab14cc33ebf
TopSteely <tobias.stahl1@gmx.de>
2014-06-21 17:34:14 +0200

    threshold search added

--- src/modules/classifier/classifier.py
+++ src/modules/classifier/classifier.py
@@ -6,8 +6,8 @@ class StrongClassifier(object):
     def __init__(self, training_stream, feature_holder, alpha, beta, gamma, layers=10, sample_count=20):
         self.training_stream = training_stream
         self.feature_holder = feature_holder
-        self.alpha = alpha
-        self.beta = beta
+        self.A = (1. - beta) / alpha
+        self.B = beta / (1. - alpha)
         self.gamma = gamma
         self.layers = layers
         self.all_classifiers = []  # len(classifiers) = T
@@ -53,20 +53,17 @@ class StrongClassifier(object):
         for patch in s:
             weighted_patches[patch] = 1. / len(s)
 
-        A = (1 - self.beta) / self.alpha
-        B = self.beta / (1 - self.alpha)
-
         for t in range(self.layers+1):
             # choose the weak classifier with the minimum error
             h_t = self._fetch_best_weak_classifier(weighted_patches)
             self.classifiers.append(h_t)    # add it to the strong classifier
 
-            self._estimate_ratio(weighted_patches, t)
+            neg, pos = self._estimate_ratio(weighted_patches, t)
             # find decision thresholds for the strong classifier
-            self._tune_thresholds(t)
+            self._tune_thresholds(t, pos, neg)
 
             # throw away training samples that fall in our thresholds
-
+            
             # sample new training data
         return
 
@@ -91,13 +88,44 @@ class StrongClassifier(object):
                 else:
                     neg_responses = np.vstack((neg_responses, [response, true_label, w]))
         # Compute Cumulative conditional probabilities of classes
-        plot_histograms(pos_responses, neg_responses)
+        #plot_histograms(pos_responses, neg_responses)
+        data = np.append(pos_responses, neg_responses, axis=0)
+        plot_gaussians(data, 0.5, 0.5)
+        # compute gaussians for negatve and positive classes
+        sigma_neg = np.std(neg_responses)
+        sigma_pos = np.std(pos_responses)
+        h_neg = 1.144 * sigma_neg * len(neg_responses) ** -0.2
+        h_pos = 1.144 * sigma_pos * len(pos_responses) ** -0.2
+        lin_space = np.linspace(-1, 1, num=1000)
+        neg_sum_of_gaussians = sum_of_gaussians(neg_responses, lin_space, h_neg)
+        pos_sum_of_gaussians = sum_of_gaussians(pos_responses, lin_space, h_pos)
+        return neg_sum_of_gaussians, pos_sum_of_gaussians
 
 
 
-    def _tune_thresholds(self, layer):
+
+    def _tune_thresholds(self, layer, pos_gaussian, neg_gaussian):
         """ Update the threshold of the classifier """
-        pass
+        # find thresholds according to http://personal.ee.surrey.ac.uk/Personal/Z.Kalal/Publications/2007_MSc_thesis.pdf
+        # page 37, 38
+        index = 0
+        for theta_a_candidate in np.linspace(-1, 1, num=1000):
+            if neg_gaussian[index] < theta_a_candidate and pos_gaussian[index] < theta_a_candidate:
+                r_a = neg_gaussian[index] / pos_gaussian[index]
+                if r_a > self.A:
+                    break
+            index += 1
+        lin_space = np.linspace(1, -1, num=1000)  # from right to left
+        index = 0
+        for theta_b_candidate in lin_space:
+            if neg_gaussian[index] > theta_b_candidate and pos_gaussian[index] > theta_b_candidate:
+                r_b = neg_gaussian[index] / pos_gaussian[index]
+                if r_b < self.B:
+                    break
+            index += 1
+        self.classifiers[layer].theta_a = theta_a_candidate
+        self.classifiers[layer].theta_b = theta_b_candidate
+
 
     def _fetch_best_weak_classifier(self, weighted_patches):
         """ Returns the weak classifier that produces the least error
--- src/modules/util/math_f.py
+++ src/modules/util/math_f.py
@@ -3,13 +3,11 @@ import matplotlib.pyplot as plt
 
 
 def plot_histograms(pos_labeled_data, neg_labeled_data):
-    height = pos_labeled_data[:, 0]
     # hist, bins = np.histogram(height, 50)
     # center = (min(pos_labeled_data[:, 1]) + max(pos_labeled_data[:, 1])) / 2
-    plt.hist(height, bins=10)
+    plt.hist(pos_labeled_data[:, 0], bins=50, alpha=.5, color='blue')
+    plt.hist(neg_labeled_data[:, 0], bins=50, alpha=.5, color='red')
     plt.show()
-    # plt.bar(center, hist, align='center', width=99)
-    # plt.show()
 
 
 def append_gaussian(gmm, gaussian, lin_space):
@@ -62,8 +60,8 @@ def plot_gaussians(data, sigma, h):
     negatives = data[data[:, 1] == -1]
     # plot individual kernels
     fig, ax = plt.subplots(2, 1, sharex=True, sharey=True)
-    ax[0].hist(positives[:, 0], normed=True, alpha=0.5, label='Histogram of positives')
-    ax[1].hist(negatives[:, 0], normed=True, alpha=0.5, label='Histogram of negatives')
+    ax[0].hist(positives[:, 0], normed=True, alpha=.5, label='Histogram of positives')
+    ax[1].hist(negatives[:, 0], normed=True, alpha=.5, label='Histogram of negatives')
     first = True
     for xx in positives:
         x = xx[0]
@@ -85,10 +83,11 @@ def plot_gaussians(data, sigma, h):
         else:
             ax[1].plot(kernel_xs, kernel_ys, 'r--', linewidth=1)
     # plot kernel density estimator (sum of individual kernels)
-    min_neg = min(negatives[:, 0]) - 20
-    max_neg = max(negatives[:, 0]) + 20
-    min_pos = min(positives[:, 0]) - 20
-    max_pos = max(positives[:, 0]) + 20
+    margin = max(max(negatives[:, 0])-min(negatives[:, 0]), max(positives[:, 0])-min(positives[:, 0]))
+    min_neg = min(negatives[:, 0]) - margin
+    max_neg = max(negatives[:, 0]) + margin
+    min_pos = min(positives[:, 0]) - margin
+    max_pos = max(positives[:, 0]) + margin
     xs_n = np.linspace(min_neg, max_neg, 100)
     xs_p = np.linspace(min_pos, max_pos, 100)
     kde_n = sum_of_gaussians(negatives[:, 0], xs_n, h)
